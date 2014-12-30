b30505d6edb2086673440ed05f9a1a2b988b37c8
kat.hidekazu.1006@googlemail.com <kato.hidekazu.1006@googlemail.com>
2014-12-30 20:29:18 +0900

    auth added

---
+++ app/Model/User.php
@@ -0,0 +1,38 @@
+<?php
+App::uses('AppModel', 'Model');
+App::uses('SimplePasswordHasher', 'Controller/Component/Auth');
+
+/**
+ * Description of User
+ *
+ * @author HK
+ */
+class User extends AppModel {
+    public $validate = array(
+        'username' => array(
+          'required' => array(
+              'rule' => 'notEmpty',
+              'message' => 'Please enter a username'
+          )
+        ),
+        'password' => array(
+            'required' => array(
+                'rule' => 'notEmpty',
+                'message' => 'Please enter a password'
+            )
+        )
+    );
+    
+    public function beforeSave($options = array()) {
+        if (!parent::beforeSave($options)) {
+            return false;
+        }
+        
+        if (isset($this->data[$this->alias]['password'])) {
+            $hasher = new SimplePasswordHasher();
+            $this->data[$this->alias]['password'] = $hasher->has($this->data[$this->alias]['password']);
+        }
+        
+        return true;
+    }
+}
--- create_table.sql
+++ create_table.sql
@@ -7,3 +7,18 @@ create table products (
 	modified datetime,
 	primary key(id)
 );
+
+insert into products (id, name, details, available, created, modified)
+	values
+	('6f55edae-fbab-4a71-989f-abd02553e719', 'Cake', 'Yummy and sweet', 1, NOW(), NOW()),
+	('e1e07536-463e-4f06-aae1-d55b11628ddd', 'Cookie', 'Browsers love cookie', 1, NOW(), NOW()),
+	('ed609a63-f3e8-4dec-9fd1-81959ff33faf', 'Helper', 'Helping you all the way', 1, NOW(), NOW());
+
+create table users (
+	id varchar(36) not null,
+	username varchar(20),
+	password varchar(100),
+	created datetime,
+	modified datetime,
+	primary key(id)
+);	
