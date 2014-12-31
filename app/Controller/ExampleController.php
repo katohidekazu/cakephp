d8454e5f2f0350fece0967e2ddbfa3471cbc9e1d
kat.hidekazu.1006@googlemail.com <kato.hidekazu.1006@googlemail.com>
2014-12-31 11:31:28 +0900

    response test added

--- app/Controller/ExampleController.php
+++ app/Controller/ExampleController.php
@@ -11,4 +11,15 @@ class ExampleController extends AppController {
     public function language() {
         $this->set('language', Configure::read('Config.language'));
     }
+    
+    public function response() {
+        if (!$this->request->query('text')) {
+            $this->response->statusCode(404);
+        }
+        $this->response->header('X-Timestamp', date('H:i:s'));
+        $this->response->type('txt');
+        $this->response->body($this->request->query('text'));
+        $this->response->send();
+        $this->_stop();
+    }
 }
